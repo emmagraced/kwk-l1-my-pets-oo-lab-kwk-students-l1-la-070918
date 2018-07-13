53f6e3d5f46550fe687c8f399252c982ceb52921
Emma Davis <emmadavis01@gmail.com>
2018-07-13 16:24:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-  # code goes here
+  attr_accessor :name, :mood, :age, :breed
+ def initialize(name, mood, age, breed)
+   @name = name
+   
+   @mood = mood
+   
+   @age = age
+   
+   @breed = breed
+ end
 end
+dog_1=Dog.new("Champion", "Happy", "24", "Pitbull")
+puts dog_1.name
+puts dog_1.mood
+puts dog_1.age
+puts dog_1.breed
+
+
